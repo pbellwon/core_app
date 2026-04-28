0c3a75dddaeac96b734aaf4a9b457dd7ae611bcf
pbellwon <pawelbellwon@gmail.com>
2026-04-28 21:25:42 +0200

    d19 - Kartra help desk implementation

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
+
 // ignore: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:html' as html;
-import 'package:flutter/foundation.dart';
 
 import 'widgets/main_app_bar.dart';
 import 'providers/menu_provider.dart';
@@ -15,58 +16,85 @@ class HelpPage extends StatefulWidget {
 }
 
 class _HelpPageState extends State<HelpPage> {
+  static bool _kartraInjected = false;
+
   @override
   void initState() {
     super.initState();
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<MenuProvider>(context, listen: false)
-          .setCurrentPage('Help');
+      Provider.of<MenuProvider>(context, listen: false).setCurrentPage('Help');
     });
+
+    if (kIsWeb && !_kartraInjected) {
+      _kartraInjected = true;
+      _injectKartra();
+    }
+  }
+
+  void _injectKartra() {
+    // 1. CSS do <head>
+    if (html.document.head!.querySelector('#kartra-css') == null) {
+      final link = html.LinkElement()
+        ..id = 'kartra-css'
+        ..rel = 'stylesheet'
+        ..type = 'text/css'
+        ..href = 'https://app.kartra.com/css/new/css/kartra_helpdesk_sidebar_out.css';
+      html.document.head!.append(link);
+    }
+
+    // 2. Kontener + skrypt + przycisk – dokładnie jak w oryginalnym kodzie Kartra
+    if (html.document.getElementById('kartra_live_chat') == null) {
+      final container = html.DivElement()
+        ..setAttribute('rel', 'JI7Z4DfvsCZa')
+        ..setAttribute('article', '')
+        ..setAttribute('product', '')
+        ..setAttribute('embedded', '1')
+        ..id = 'kartra_live_chat'
+        ..className = 'kartra_helpdesk_sidebar js_kartra_trackable_object';
+
+      // Skrypt WEWNĄTRZ kontenera (wymagane przez Kartra)
+      final script = html.ScriptElement()
+        ..type = 'text/javascript'
+        ..src = 'https://app.kartra.com/resources/js/helpdesk_frame';
+      container.append(script);
+
+      // Przycisk helpdesku
+      final button = html.DivElement()
+        ..setAttribute('rel', 'JI7Z4DfvsCZa')
+        ..id = 'display_kartra_helpdesk'
+        ..className = 'kartra_helpdesk_sidebar_button open';
+      container.append(button);
+
+      html.document.body!.append(container);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (kIsWeb) {
-      // Rejestracja iframe tylko na web
-      // ignore: undefined_prefixed_name
-      // ignore: avoid_web_libraries_in_flutter
-      final String viewID = 'kartra-helpdesk-iframe';
-      // ignore: undefined_prefixed_name
-      html.Element? existing = html.document.getElementById(viewID);
-      if (existing == null) {
-        final iframe = html.IFrameElement()
-          ..id = viewID
-          ..srcdoc = '''
-            <html>
-              <head>
-                <link rel="stylesheet" type="text/css" href="https://app.kartra.com/css/new/css/kartra_helpdesk_sidebar_out.css" />
-              </head>
-              <body>
-                <div rel="JI7Z4DfvsCZa" article="" product="" embedded="1" id="kartra_live_chat" class="kartra_helpdesk_sidebar js_kartra_trackable_object">
-                  <script type="text/javascript" src="https://app.kartra.com/resources/js/helpdesk_frame"></script>
-                  <div rel="JI7Z4DfvsCZa" id="display_kartra_helpdesk" class="kartra_helpdesk_sidebar_button open"></div>
-                </div>
-              </body>
-            </html>
-          '''
-          ..style.border = 'none'
-          ..style.width = '100%'
-          ..style.height = '100vh';
-        html.document.body!.append(iframe);
-      }
-      return Scaffold(
-        appBar: const MainAppBar(title: "", showBackButton: true),
-        body: const SafeArea(
-          child: HtmlElementView(viewType: 'kartra-helpdesk-iframe'),
+    return Scaffold(
+      appBar: const MainAppBar(title: "", showBackButton: true),
+      body: const SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.headset_mic_outlined, size: 64, color: Color(0xFF860E66)),
+              SizedBox(height: 24),
+              Text(
+                'Need help?',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Click the helpdesk button on the right side,\nto contact support.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15, color: Colors.grey),
+              ),
+            ],
+          ),
         ),
-      );
-    } else {
-      return Scaffold(
-        appBar: const MainAppBar(title: "", showBackButton: true),
-        body: const Center(
-          child: Text('Live chat dostępny tylko w wersji web.'),
-        ),
-      );
-    }
+      ),
+    );
   }
 }
